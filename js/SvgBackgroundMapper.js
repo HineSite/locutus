291c21b380f3859af5552e0cfc5904d3a04e5966
Kevin Hine <45086738+HineSite@users.noreply.github.com>
2021-09-26 17:42:10 -0400

    Replaced multiple variables with variable holder objects to make things easier and cleaner.

--- js/SvgBackgroundMapper.js
+++ js/SvgBackgroundMapper.js
@@ -1,42 +1,34 @@
 export class SvgCircleItem {
     #element = HTMLElement;
 
-    #orgR = 0;
-    #orgX = 0;
-    #orgY = 0;
-
-    #r = 0;
-    #x = 0;
-    #y = 0;
-
-    #scaledR = 0;
-    #scaledX = 0;
-    #scaledY = 0;
+    #org = {};
+    #current = {};
+    #scaled = {};
 
     #scale = 0;
     #offsets = {};
 
     constructor(element) {
         this.#element = element;
-        this.#orgR = element.getAttribute('r');
-        this.#orgX = element.getAttribute('cx');
-        this.#orgY = element.getAttribute('cy');
+        this.#org = this.#makeVariables(
+            element.getAttribute('r'),
+            element.getAttribute('cx'),
+            element.getAttribute('cy')
+        );
 
-        this.#r = this.#orgR;
-        this.#x = this.#orgX;
-        this.#y = this.#orgY;
+        Object.assign(this.#current, this.#org);
     }
 
     // Update the position of the circle based on real(?) coordinates.
     updatePosition(r, x, y) {
         if (r)
-            this.#r = r;
+            this.#current.r = r;
 
         if (x)
-            this.#x = x;
+            this.#current.x = x;
 
         if (y)
-            this.#y = y;
+            this.#current.y = y;
 
         this.updateView(this.#scale, this.#offsets);
     }
@@ -44,15 +36,15 @@ export class SvgCircleItem {
     // Update the position of the circle based on the current scaled coordinates.
     updateScaledPosition(r, x, y) {
         if (r) {
-            this.#r = (r / this.#scale);
+            this.#current.r = (r / this.#scale);
         }
 
         if (x) {
-            this.#x = (x + this.#offsets.x) / this.#scale;
+            this.#current.x = (x + this.#offsets.x) / this.#scale;
         }
 
         if (y) {
-            this.#y = (y + this.#offsets.y) / this.#scale;
+            this.#current.y = (y + this.#offsets.y) / this.#scale;
         }
 
         this.updateView(this.#scale, this.#offsets);
@@ -63,74 +55,69 @@ export class SvgCircleItem {
         this.#scale = scale;
         this.#offsets = offsets;
 
-        this.#scaledR = (this.#r * scale);
-        this.#scaledX = ((this.#x * scale) - offsets.x);
-        this.#scaledY = ((this.#y * scale) - offsets.y);
+        this.#scaled.r = (this.#current.r * scale);
+        this.#scaled.x = ((this.#current.x * scale) - offsets.x);
+        this.#scaled.y = ((this.#current.y * scale) - offsets.y);
 
-        this.#element.setAttribute('r', this.#scaledR);
-        this.#element.setAttribute('cx', this.#scaledX);
-        this.#element.setAttribute('cy', this.#scaledY);
+        this.#element.setAttribute('r', this.#scaled.r);
+        this.#element.setAttribute('cx', this.#scaled.x);
+        this.#element.setAttribute('cy', this.#scaled.y);
     }
 
     // Resets the current coords to the original coords.
     resetPosition() {
-        this.#r = this.#orgR;
-        this.#x = this.#orgX;
-        this.#y = this.#orgY;
+        Object.assign(this.#current, this.#org);
 
         this.updateView(this.#scale, this.#offsets);
+    }
+
+    #makeVariables (r, x, y) {
+        return {
+            r: r,
+            x: x,
+            y: y
+        };
     }
 }
 
 export class SvgLineItem {
     #element = HTMLElement;
-    #orgX1 = 0;
-    #orgY1 = 0;
-    #orgX2 = 0;
-    #orgY2 = 0;
 
-    #x1 = 0;
-    #y1 = 0;
-    #x2 = 0;
-    #y2 = 0;
-
-    #scaledX1 = 0;
-    #scaledY1 = 0;
-    #scaledX2 = 0;
-    #scaledY2 = 0;
+    #org = {};
+    #current = {};
+    #scaled = {};
 
     #scale = 0;
     #offsets = {};
 
     constructor(element) {
         this.#element = element;
-        this.#orgX1 = element.getAttribute('x1');
-        this.#orgY1 = element.getAttribute('y1');
-        this.#orgX2 = element.getAttribute('x2');
-        this.#orgY2 = element.getAttribute('y2');
+        this.#org = this.#makeVariables(
+            element.getAttribute('x1'),
+            element.getAttribute('y1'),
+            element.getAttribute('x2'),
+            element.getAttribute('y2')
+        );
 
-        this.#x1 = this.#orgX1;
-        this.#y1 = this.#orgY1;
-        this.#x2 = this.#orgX2;
-        this.#y2 = this.#orgY2;
+        Object.assign(this.#current, this.#org);
     }
 
     // Update the position of the circle based on real(?) coordinates.
     updatePosition(x1, y1, x2, y2) {
         if (x1) {
-            this.#x1 = x1;
+            this.#current.x1 = x1;
         }
 
         if (y1) {
-            this.#y1 = y1;
+            this.#current.y1 = y1;
         }
 
         if (x2) {
-            this.#x2 = x2;
+            this.#current.x2 = x2;
         }
 
         if (y2) {
-            this.#y2 = y2;
+            this.#current.y2 = y2;
         }
 
         this.updateView(this.#scale, this.#offsets);
@@ -139,19 +126,19 @@ export class SvgLineItem {
     // Update the position of the circle based on the current scaled coordinates.
     updateScaledPosition(x1, y1, x2, y2) {
         if (x1) {
-            this.#x1 = (x1 + this.#offsets.x) / this.#scale;
+            this.#current.x1 = (x1 + this.#offsets.x) / this.#scale;
         }
 
         if (y1) {
-            this.#y1 = (y1 + this.#offsets.y) / this.#scale;
+            this.#current.y1 = (y1 + this.#offsets.y) / this.#scale;
         }
 
         if (x2) {
-            this.#x2 = (x2 + this.#offsets.x) / this.#scale;
+            this.#current.x2 = (x2 + this.#offsets.x) / this.#scale;
         }
 
         if (y2) {
-            this.#y2 = (y2 + this.#offsets.y) / this.#scale;
+            this.#current.y2 = (y2 + this.#offsets.y) / this.#scale;
         }
 
         this.updateView(this.#scale, this.#offsets);
@@ -162,25 +149,31 @@ export class SvgLineItem {
         this.#scale = scale;
         this.#offsets = offsets;
 
-        this.#scaledX1 = ((this.#x1 * scale) - offsets.x);
-        this.#scaledY1 = ((this.#y1 * scale) - offsets.y);
-        this.#scaledX2 = ((this.#x2 * scale) - offsets.x);
-        this.#scaledY2 = ((this.#y2 * scale) - offsets.y);
+        this.#scaled.x1 = ((this.#current.x1 * scale) - offsets.x);
+        this.#scaled.y1 = ((this.#current.y1 * scale) - offsets.y);
+        this.#scaled.x2 = ((this.#current.x2 * scale) - offsets.x);
+        this.#scaled.y2 = ((this.#current.y2 * scale) - offsets.y);
 
-        this.#element.setAttribute('x1', this.#scaledX1);
-        this.#element.setAttribute('y1', this.#scaledY1);
-        this.#element.setAttribute('x2', this.#scaledX2);
-        this.#element.setAttribute('y2', this.#scaledY2);
+        this.#element.setAttribute('x1', this.#scaled.x1);
+        this.#element.setAttribute('y1', this.#scaled.y1);
+        this.#element.setAttribute('x2', this.#scaled.x2);
+        this.#element.setAttribute('y2', this.#scaled.y2);
     }
 
     // Resets the current coords to the original coords.
     resetPosition() {
-        this.#x1 = this.#orgX1;
-        this.#y1 = this.#orgY1;
-        this.#x2 = this.#orgX2;
-        this.#y2 = this.#orgY2;
+        Object.assign(this.#current, this.#org);
 
         this.updateView(this.#scale, this.#offsets);
+    }
+
+    #makeVariables (x1, y1, x2, y2) {
+        return {
+            x1: x1,
+            y1: y1,
+            x2: x2,
+            y2: y2
+        };
     }
 }
 
